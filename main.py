28669d4c14f4a297e2db3668a522ea7f88803ef9
AmineSAAD-EDDINE <amine.saad-eddine@edu.esiee.fr>
2024-09-16 12:02:01 +0000

    les erreurs que j'ai pu rencontrer pour améliorer mon code était l'oubli de retrait d'espaces inutiles et de mettre des docstrings afin d'améliorer la lisibilité de mes programmes.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #### Fonction secondaire
 
-
+"""cette fonction permet de savoir si un mot ou une phrase est un palindrome"""
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
+    """cette fonction permet de savoir si un mot ou une phrase est un palindrome"""
+    p=str.lower(p)
+    ent = "àçéèëêiôù-'^!§:/;.,?&#([-|_)]=)"
+    sor = "aceeeeiou                      "
+    trans = str.maketrans(ent,sor)
+    mot = p.translate(trans)
+    motsansaccent = mot.replace(" ", "")
+    for i in range (len(motsansaccent)//2):
+        if motsansaccent[i]!=motsansaccent[-i-1]:
+            return False
+    return True
 #### Fonction principale
 
-
 def main():
+    """cette fonction principale permet d'effectuer des appels de la fonction ispalindrome"""
 
     # vos appels à la fonction secondaire ici
 
